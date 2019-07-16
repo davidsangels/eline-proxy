e9ec72b24b9fb57df32fff30081f934aa94e10d6
Elinex <eline.ax@gmail.com>
2019-07-15 19:57:53 -0700

    all the services running in the proxy

--- server/index.js
+++ server/index.js
@@ -2,29 +2,35 @@ var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var reviewsServer = 'http://localhost:3001';
+var reservation = 'http://localhost:3001';
+var imgsGallery = 'http://localhost:3002';
+var reviews = 'http://localhost:3003';
 const port = 3000;
 
 app.use(express.static('public'));
 
-// Get all idPlaces
+// RESERVATION:
+app.get('/bookings', (req, res) => {
+  apiProxy.web(req, res, {target: reservation});
+});
+
+// IMAGES GALLERY:
+app.get('/data', (req, res) => {
+  apiProxy.web(req, res, {target: imgsGallery});
+});
+
+// REVIEWS SERVICE:
 app.get('/api/places', (req, res) => {
-  apiProxy.web(req, res, {target: reviewsServer});
-})
-
-// Get all the reviews for a specific place
+  apiProxy.web(req, res, {target: reviews});
+});
 app.get('/api/reviews/:idPlace', (req, res) => {
-  apiProxy.web(req, res, {target: reviewsServer});
-})
-
-// Get all the ratings for a specific place
+  apiProxy.web(req, res, {target: reviews});
+});
 app.get('/api/ratings/:idPlace', (req, res) => {
-  apiProxy.web(req, res, {target: reviewsServer});
-})
-
-// Get all the reviews for a specific query and a specific idPlace
+  apiProxy.web(req, res, {target: reviews});
+});
 app.get('/api/reviews/search/:idPlace/:query', (req, res) => {
-  apiProxy.web(req, res, {target: reviewsServer});
-})
+  apiProxy.web(req, res, {target: reviews});
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
