41e0491baf248502d2e2db614abeb96c058dff58
Elinex <eline.ax@gmail.com>
2019-07-16 18:17:45 -0700

    changes to make run reviews service with the new changes in it

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var reservation = 'http://localhost:3001';
-var imgsGallery = 'http://localhost:3002';
+// var reservation = 'http://localhost:3001';
+// var imgsGallery = 'http://localhost:3002';
 var reviews = 'http://localhost:3003';
 const port = 3000;
 
-app.use(express.static('public'));
+app.use('/:id', express.static('public'));
 
 // RESERVATION:
 app.get('/bookings', (req, res) => {
@@ -20,16 +20,13 @@ app.get('/data', (req, res) => {
 });
 
 // REVIEWS SERVICE:
-app.get('/api/places', (req, res) => {
+app.get('/reviews/:idPlace', (req, res) => {
   apiProxy.web(req, res, {target: reviews});
 });
-app.get('/api/reviews/:idPlace', (req, res) => {
+app.get('/reviews/ratings/:idPlace', (req, res) => {
   apiProxy.web(req, res, {target: reviews});
 });
-app.get('/api/ratings/:idPlace', (req, res) => {
-  apiProxy.web(req, res, {target: reviews});
-});
-app.get('/api/reviews/search/:idPlace/:query', (req, res) => {
+app.get('/reviews/search/:idPlace/:query', (req, res) => {
   apiProxy.web(req, res, {target: reviews});
 });
 
